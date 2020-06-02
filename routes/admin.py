41601c1e0c30f29ac6d47a2abaaab11eb9b97036
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-06-02 14:04:37 +0300

    Customizing the admin area

--- routes/admin.py
+++ routes/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import Route
 
-admin.site.register(Route)
+
+class RouteAdmin(admin.ModelAdmin):
+    """ Displaying data in the admin page """
+
+    list_display = ('id', 'route_from', 'route_to', 'sacco_name',
+                    'morning_fare', 'daytime_fare', 'evening_fare')
+    list_display_links = ('id', 'route_from',)
+    list_filter = ('sacco_name',)
+    search_fields = ('route_from', 'route_to', 'sacco_name')
+    list_per_page = 30
+
+
+admin.site.register(Route, RouteAdmin)
