77b3f04820ef116a33a3f5a2ef8b1555b39b51ae
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-06-04 15:15:38 +0300

    Updated settings and requirements

--- mat/settings.py
+++ mat/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 from django.contrib.messages import constants as messages
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ STATICFILES_DIRS = [
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+django_heroku.settings(locals())
