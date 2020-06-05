b6bfb11ab64e1d045ee8401d68c5328fd992aaba
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-06-05 20:06:49 +0300

    Updated contacts list display

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,7 +4,8 @@ from .models import Contact
 
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'contact_date', 'message')
+    list_display = ('id', 'name', 'route_from',
+                    'route_to', 'fare', 'sacco_ame')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'email')
     list_per_page = 30
